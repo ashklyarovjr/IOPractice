ae542aa5f7cf281f2bc323f51632f05bbf55ca1a
ashklyarovjr <anton.shklyarov@gmail.com>
2015-05-05 16:48:08 +0300

    Comments added

--- src/FileDemo.java
+++ src/FileDemo.java
@@ -1,17 +1,24 @@
 import java.io.*;
-
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
 
 
 public class FileDemo {
     public static void main(String[] args) throws IOException {
-        File file = new File("D:/java");
-        File file1 = new File("D:/temp/java_copy");
+        File fileFrom = new File("D:/java");
+        File fileTo = new File("D:/temp/java_copy");
 
-        copyDir(file,file1);
+        copyDir(fileFrom, fileTo);
 
 
     }
 
+
+    /**
+     * Copying content from one file to another
+     * @param source - File path we're copying from
+     * @param target - File path we're copying to
+     * */
     public static void copyFile(File source, File target) throws IOException {
         if (!source.isDirectory()) {
             try (InputStream input = new FileInputStream(source);
@@ -23,14 +30,18 @@ public class FileDemo {
                 while ((bytesRead = input.read(buf)) > 0) {
                     output.write(buf, 0, bytesRead);
                 }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
             }
         }
     }
 
+
+    /**
+     * Copying a directory
+     * @param source - Directory we're copying from
+     * @param target - Directory we're copying to
+     * */
     public static void copyDir(File source, File target) throws IOException {
-        if(!source.exists())
+        if (!source.exists())
             return;
         if (source.isDirectory()) {
             if (!target.exists()) {
@@ -43,13 +54,32 @@ public class FileDemo {
                 copyDir(srcFile, destFile);
             }
         } else {
-            copyFile(source,target);
+            copyFile(source, target);
         }
 
     }
 
-    public static void copyFileWithNIO(File source, File target) {
 
+    /**
+     * Copying a directory with NIO lib
+     * @param source - Directory we're copying from
+     * @param target - Directory we're copying to
+     * */
+    public static void copyFileWithNIO(File source, File target) throws IOException {
+        try (FileChannel channelin = new FileInputStream(source).getChannel();
+             FileChannel channelout = new FileOutputStream(target).getChannel()) {
+            channelout.transferFrom(channelin, 0, channelin.size());
+        }
+    }
+
+
+    /**
+     * Copying a directory with Java 7
+     * @param source - Directory we're copying from
+     * @param target - Directory we're copying to
+     * */
+    public static void copyFilesWithJava7(File source, File target) throws IOException {
+        Files.copy(source.toPath(),target.toPath());
     }
 }
 
